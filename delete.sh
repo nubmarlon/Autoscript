ef5fbae2f6f40a3701a3e8537fda00f905d7d2d5
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-07-28 03:52:34 +0800

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Script to remove user SSH & OpenVPN
 
-read -p "Name user SSH which will be deleted: "Users
+read -p "Name user SSH which will be deleted: " Users
 
-if getent passwd $Users > /dev/null 2>&1; then
-        userdel $Users
-        echo -e "User $Users has been deleted."
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna has been deleted."
 else
-        echo -e "FAILED: User $Users does not exist"
+        echo -e "FAILED: User $Pengguna does not exist"
 fi
