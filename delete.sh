4805cc9df826769b9c13e880bb423458703543bc
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-07-28 02:24:15 +0800

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -5,7 +5,7 @@ read -p "Name user SSH which will be deleted: "Users
 
 if getent passwd $Users > /dev/null 2>&1; then
         userdel $Users
-        echo -e "User $User has been deleted."
+        echo -e "User $Users has been deleted."
 else
-        echo -e "FAILED: User $User does not exist"
+        echo -e "FAILED: User $Users does not exist"
 fi
