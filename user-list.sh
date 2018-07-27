42f07c70e9942c10521d32f24cc827365ff328b5
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-07-28 02:34:17 +0800

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -4,14 +4,14 @@ echo "USERNAME          EXP DATE     " | lolcat
 echo "-------------------------------" | lolcat
 while read expired
 do
-        AKUN="$(echo $expired | cut -d: -f1)"
+        ACCOUNT="$(echo $expired | cut -d: -f1)"
         ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        exp="$(chage -l $ACCOUNT | grep "Account expires" | awk -F": " '{print $2}')"
         if [[ $ID -ge 1000 ]]; then
-        printf "%-17s %2s\n" "$AKUN" "$exp"
+        printf "%-17s %2s\n" "$ACCOUNT" "$exp"
         fi
 done < /etc/passwd
-JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+TOTAL="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "-------------------------------" | lolcat
 echo "Account number: $TOTAL user"
 echo "-------------------------------" | lolcat
